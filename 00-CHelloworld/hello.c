3da5929974c433aa4ca22db3d1808502305d9776
dregojo <dregojo@frba.utn.edu.ar>
2025-04-08 16:36:36 -0300

    Update hello.c
    Se agrega comentarios

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -1,3 +1,8 @@
+/* TP0 "Hello, World!"
+ * Se codifica un programa básico en donde se imprime por pantalla la frase "Hello, World" en lenguaje C y luego se redirecciona como salida hacia un archivo "output.txt"
+ * Autor: Dario Joaquin Regojo
+ * Ultima modificacion: 20250408
+*/
 #include <stdio.h>
 
 int main(){
